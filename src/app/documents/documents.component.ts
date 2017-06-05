1a620b1e14a6395a6275fd0a655181e85997fe5f
Joseph Murphy <jmrphyii@memphis.edu>
2017-06-04 23:50:08 -0500

    I have no idea bootstrap stuff

---
+++ src/app/documents/documents.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Document } from './document'
+
+@Component({
+    moduleId: module.id,
+    selector: 'documents',
+    templateUrl: './documents.component.html',
+	styleUrls: [ './documents.component.css']
+})
+export class DocumentsComponent{
+    pageTitle: string = 'Docement Dashboard'
+    documents: Document[] = [
+		{
+			title: "My First Doc",
+			description: 'asdfasdfasdf asdfasd',
+			file_url: 'http://google.com',
+			updated_at: '11/11/16',
+			image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
+		},
+		{
+			title: "My Second Doc",
+			description: 'asdfasdfasdf asdfasd',
+			file_url: 'http://google.com',
+			updated_at: '11/11/16',
+			image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
+		},
+		{
+			title: "My Last Doc",
+			description: 'asdfasdfasdf asdfasd',
+			file_url: 'http://google.com',
+			updated_at: '11/11/16',
+			image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
+		}
+	]
+}
